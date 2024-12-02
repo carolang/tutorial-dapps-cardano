5cbcc9745d3b67dc616f0185b8ec8c5fde21328b
Carolina Lang <clang@eryx.co>
2024-12-02 16:42:35 +0000

    Aplicacion NettJS andando

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
